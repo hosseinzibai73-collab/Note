d41b350791f229c54c5f5d1774d71c5e49e34d7b
hosseinzibai73-collab <hosseinzibai73@gmail.com>
2026-09-21 02:04:17 +0330

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+var CACHE_NAME = 'yaddasht-v1';
+var APP_SHELL = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(event){
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(APP_SHELL);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE_NAME; }).map(function(k){ return caches.delete(k); }));
+    }).then(function(){ return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function(event){
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      var networkFetch = fetch(event.request).then(function(resp){
+        if (resp && resp.status === 200){
+          var copy = resp.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copy); });
+        }
+        return resp;
+      }).catch(function(){ return cached; });
+      return cached || networkFetch;
+    })
+  );
+});
